5f6bb2d1c41620e80f2fdd72cfd9f3342a507817
vanessa.collado90@gmail.com <vanessa.collado90@gmail.com>
2018-03-26 18:02:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function  destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  function deleteFromObjectByKey(object, key){
  var newObj= Object.assign({}, object, key)
  
- delete newObj.key 
+ delete newObj[key] 
    
    return newObj
  }
